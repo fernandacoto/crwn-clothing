ad333d69f2a8585ee7db90f3a82b0ab953bffd8e
Fernanda Coto <fermarioro@gmail.com>
2021-09-28 22:13:56 -0600

    Update shop data so that ids are not the same on diff categories, add cart utils to group cart items

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 import './collection-item.styles.scss';
 
-const CollectiionItem = ({id, name, price, imageUrl}) => (
-  <div className='collection-item'>
-    <div className='image' style={{backgroundImage: `url(${imageUrl})`}}/>
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
+const CollectiionItem = ({item, addItem}) => {
+  const { imageUrl, name, price } = item;
+  return(
+    <div className='collection-item'>
+      <div className='image' style={{backgroundImage: `url(${imageUrl})`}}/>
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <CustomButton inverted onClick={() => addItem(item)}>Add to cart</CustomButton>
     </div>
-  </div>
-)
+  );
+}
+const mapDispatchToProps = dispatch => ({
+  addItem: (item) => dispatch(addItem(item))
+})
 
-export default CollectiionItem;
+export default connect(null, mapDispatchToProps)(CollectiionItem);
